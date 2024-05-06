e6d369f186936b03e1a380292546d368d392cb84
Wim Haeck <whaeck@gmail.com>
2024-05-06 11:07:29 -0600

    Adding more digits to CrossSection test

--- src/NDItk/multigroup/CrossSection/test/CrossSection.test.cpp
+++ src/NDItk/multigroup/CrossSection/test/CrossSection.test.cpp
@@ -28,7 +28,7 @@ SCENARIO( "CrossSection" ) {
 
       int reaction = 2;
       double qvalue = 0.0;
-      std::vector< double > values = { 10., 20., 30., 40., 50., 60., 70. };
+      std::vector< double > values = { 10.0000000000001, 20., 30., 40.12345678, 50., 60., 70. };
 
       CrossSection chunk( reaction, qvalue, std::move( values ) );
 
@@ -113,24 +113,24 @@ SCENARIO( "CrossSection" ) {
 std::string chunk() {
 
   return "    2 0\n"
-         "    10 20 30 40 50\n"
+         "    10.0000000000001 20 30 40.12345678 50\n"
          "    60 70\n";
 }
 
 std::vector< double > data() {
 
   return { 2, 0,
-           10, 20, 30, 40, 50, 60, 70 };
+           10.0000000000001, 20, 30, 40.12345678, 50, 60, 70 };
 }
 
 void verifyChunk( const CrossSection& chunk ) {
 
   CHECK_THAT(  2, WithinRel( chunk.values()[0] ) );
   CHECK_THAT(  0, WithinRel( chunk.values()[1] ) );
-  CHECK_THAT( 10, WithinRel( chunk.values()[2] ) );
+  CHECK_THAT( 10.0000000000001, WithinRel( chunk.values()[2] ) );
   CHECK_THAT( 20, WithinRel( chunk.values()[3] ) );
   CHECK_THAT( 30, WithinRel( chunk.values()[4] ) );
-  CHECK_THAT( 40, WithinRel( chunk.values()[5] ) );
+  CHECK_THAT( 40.12345678, WithinRel( chunk.values()[5] ) );
   CHECK_THAT( 50, WithinRel( chunk.values()[6] ) );
   CHECK_THAT( 60, WithinRel( chunk.values()[7] ) );
   CHECK_THAT( 70, WithinRel( chunk.values()[8] ) );
@@ -141,10 +141,10 @@ void verifyChunk( const CrossSection& chunk ) {
   CHECK( 7 == chunk.numberGroups() );
   CHECK( 2 == chunk.identifier() );
   CHECK_THAT( 0, WithinRel( chunk.qvalue() ) );
-  CHECK_THAT( 10, WithinRel( chunk.crossSections()[0] ) );
+  CHECK_THAT( 10.0000000000001, WithinRel( chunk.crossSections()[0] ) );
   CHECK_THAT( 20, WithinRel( chunk.crossSections()[1] ) );
   CHECK_THAT( 30, WithinRel( chunk.crossSections()[2] ) );
-  CHECK_THAT( 40, WithinRel( chunk.crossSections()[3] ) );
+  CHECK_THAT( 40.12345678, WithinRel( chunk.crossSections()[3] ) );
   CHECK_THAT( 50, WithinRel( chunk.crossSections()[4] ) );
   CHECK_THAT( 60, WithinRel( chunk.crossSections()[5] ) );
   CHECK_THAT( 70, WithinRel( chunk.crossSections()[6] ) );

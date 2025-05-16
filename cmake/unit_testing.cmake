f0789c28eb56c7f64f1c8a4da1db5a1e427df91b
Matthew Scott Wilson <msw@ro-rfe3.lanl.gov>
2025-05-16 16:25:35 -0600

    forgot unit testing cmake

--- cmake/unit_testing.cmake
+++ cmake/unit_testing.cmake
@@ -37,6 +37,8 @@ add_subdirectory( src/NDItk/base/StringListRecord/test )
 add_subdirectory( src/NDItk/base/InformationRecord/test )
 
 add_subdirectory( src/NDItk/thermonuclear/Temperatures/test )
+add_subdirectory( src/NDItk/thermonuclear/SecondaryDistribution/test )
+add_subdirectory( src/NDItk/thermonuclear/InterpolationRegions/test )
 
 add_subdirectory( src/NDItk/depletion/Multiplicities/test )
 add_subdirectory( src/NDItk/depletion/Product/test )

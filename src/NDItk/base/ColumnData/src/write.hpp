dfe8dc1f29da7af21c5f8479bf5a754660ea71b6
Matthew Scott Wilson <msw@ro-rfe3.lanl.gov>
2025-07-15 10:45:03 -0600

    change print format

--- src/NDItk/base/ColumnData/src/write.hpp
+++ src/NDItk/base/ColumnData/src/write.hpp
@@ -10,18 +10,20 @@ void write( OutputIterator& iter ) const {
 
   auto indentLevel  = 1;
   auto indentPrefix = [](int n){ return std::string(2*n, ' '); };
+  auto maskFirst    = [](int i){ return ( (i == 0) ? 1 : 0 );  };
 
   std::ostringstream buffer;
+  buffer << std::setprecision(15);
+         
 
   auto nrow = this->size() / this->numberColumns();
 
   for (int i=0; i < nrow; ++i) {
 
-    buffer << indentPrefix(indentLevel);
-
     for (int j=0; j < this->numberColumns(); ++j) {
 
-      buffer << this->value( i*this->numberColumns() + j ) << " ";
+      buffer << indentPrefix( (indentLevel-1)*maskFirst(i) + 1 ) 
+             << this->value( i*this->numberColumns() + j );
     }
 
     buffer << "\n";
